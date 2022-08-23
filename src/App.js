d48d643fabf573ca3e76e9fdc05d13a893d1c35c
Krishna <krishnarathi341@gmail.com>
2022-08-24 00:38:44 +0530

    Completed the header section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import React, { Fragment } from "react";
+import Header from "./Components/Layout/Header";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;
